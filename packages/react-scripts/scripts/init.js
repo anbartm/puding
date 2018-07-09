db5bfdf0f0e3e173366a09a5ab6ef9bcaa329198
Srđan <srdjan.prodanovic@gmail.com>
2018-07-09 19:01:18 +0200

    Hot reloading and snapshotting

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -38,7 +38,7 @@ module.exports = function(
   // Setup the script rules
   appPackage.scripts = {
     start: 'react-scripts start',
-    build: 'react-scripts build',
+    build: 'react-scripts build && react-snapshot',
     test: 'react-scripts test --env=jsdom',
     eject: 'react-scripts eject',
   };
