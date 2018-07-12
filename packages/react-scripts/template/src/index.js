7cd1219270619c5f6c62072c5e83c7e9cd2036d1
Srđan <srdjan.prodanovic@gmail.com>
2018-07-12 19:09:15 +0200

    Refactor

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -3,33 +3,10 @@ import { render } from 'react-snapshot';
 import { Provider } from 'multiplytix-react';
 import './index.scss';
 import App from './App';
+import multiplytixConfig from './multiplytix.config.js';
 import registerServiceWorker from './registerServiceWorker';
 
 const rootEl = document.getElementById('root');
-
-const GOOGLE_ANALYTICS_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
-const GOOGLE_REMARKETING_TAG = process.env.REACT_APP_GOOGLE_REMARKETING_TAG;
-const MIXPANEL_ID = process.env.REACT_APP_MIXPANEL_ID;
-const MIXPANEL_CUSTOM_LIB_URL = process.env.REACT_APP_MIXPANEL_LIB_URL;
-const HOTJAR_ID = process.env.REACT_APP_HOTJAR_ID;
-const FACEBOOK_PIXEL_ID = process.env.REACT_APP_FACEBOOK_PIXEL_ID;
-const TWITTER_ID = process.env.REACT_APP_TWITTER_ID;
-const PINTEREST_ID = process.env.REACT_APP_PINTEREST_ID;
-const REDDIT_PIXEL_Q = process.env.REACT_APP_REDDIT_PIXEL_Q;
-const REDDIT_PIXEL_S = process.env.REACT_APP_REDDIT_PIXEL_S;
-
-const multiplytixConfig = {
-  google_analytics: GOOGLE_ANALYTICS_ID,
-  google_remarketing: GOOGLE_REMARKETING_TAG,
-  mixpanel: MIXPANEL_ID,
-  mixpanel_lib: MIXPANEL_CUSTOM_LIB_URL,
-  hotjar: HOTJAR_ID,
-  facebook_pixel: FACEBOOK_PIXEL_ID,
-  twitter_pixel: TWITTER_ID,
-  pinterest_pixel: PINTEREST_ID,
-  reddit_pixel_q: REDDIT_PIXEL_Q,
-  reddit_pixel_s: REDDIT_PIXEL_S,
-};
 
 render(
   <Provider config={multiplytixConfig}>
